024ec94e83054da6a9ca405ce1b249a037d2d60c
taesun_lee <taesun_lee@tmax.co.kr>
2021-04-08 17:08:58 +0900

    update install script

--- ovirt-4.4.3.11/ovirt-web-ui-install.sh
+++ ovirt-4.4.3.11/ovirt-web-ui-install.sh
@@ -1,11 +1,11 @@
-export BRANDING_IMAGE_DIR=./hypervm.brand/
+export BRANDING_IMAGE_DIR=./hypervm.brand/images
 
-sudo cp -i ${BRANDING_IMAGE_DIR}favicon.ico /usr/share/ovirt-web-ui/branding/images/favicon.ico
-sudo cp -i ${BRANDING_IMAGE_DIR}favicon-16x16.png /usr/share/ovirt-web-ui/branding/images/favicon-16x16.png
-sudo cp -i ${BRANDING_IMAGE_DIR}favicon-32x32.png /usr/share/ovirt-web-ui/branding/images/favicon-32x32.png
-sudo cp -i ${BRANDING_IMAGE_DIR}ovirt_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_logo.png
-sudo cp -i ${BRANDING_IMAGE_DIR}ovirt_masthead_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_masthead_logo.png
-sudo cp -i ${BRANDING_IMAGE_DIR}ovirt_middle_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_middle_logo.png
+sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}favicon.ico /usr/share/ovirt-web-ui/branding/images/favicon.ico
+sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}favicon-16x16.png /usr/share/ovirt-web-ui/branding/images/favicon-16x16.png
+sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}favicon-32x32.png /usr/share/ovirt-web-ui/branding/images/favicon-32x32.png
+sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}ovirt_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_logo.png
+sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}ovirt_masthead_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_masthead_logo.png
+sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}ovirt_middle_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_middle_logo.png
 
-sudo cp -i ./brand.css /usr/share/ovirt-web-ui/branding/brand.css
-sudo cp -i ./fixed-strings.json /usr/share/ovirt-web-ui/branding/fixed-strings.json
+sudo /usr/bin/cp -f ./brand.css /usr/share/ovirt-web-ui/branding/brand.css
+sudo /usr/bin/cp -f ./fixed-strings.json /usr/share/ovirt-web-ui/branding/fixed-strings.json
