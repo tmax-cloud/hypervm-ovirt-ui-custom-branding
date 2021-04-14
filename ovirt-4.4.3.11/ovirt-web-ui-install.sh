857415efd179559c89cd904875bb9c3e81cba06c
taesun_lee <taesun_lee@tmax.co.kr>
2021-04-14 16:26:16 +0900

    add ovirt_masthead_bg.png to installer

--- ovirt-4.4.3.11/ovirt-web-ui-install.sh
+++ ovirt-4.4.3.11/ovirt-web-ui-install.sh
@@ -6,6 +6,7 @@ sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}favicon-32x32.png /usr/share/ovirt-web-
 sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}ovirt_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_logo.png
 sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}ovirt_masthead_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_masthead_logo.png
 sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}ovirt_middle_logo.png /usr/share/ovirt-web-ui/branding/images/ovirt_middle_logo.png
+sudo /usr/bin/cp -f ${BRANDING_IMAGE_DIR}ovirt_masthead_bg.png /usr/share/ovirt-web-ui/branding/images/ovirt_masthead_bg.png
 
 sudo /usr/bin/cp -f ./brand.css /usr/share/ovirt-web-ui/branding/brand.css
 sudo /usr/bin/cp -f ./fixed-strings.json /usr/share/ovirt-web-ui/branding/fixed-strings.json
